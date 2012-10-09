9030f5238f5bcf8e008cca0b47a9fe18904391a1
Charlie Hills <public@lockshire.net>
2012-10-08 23:17:25 -0600

    Adding Timekeeper's Properties files to perforce.
    [git-p4: depot-paths = "//depot/timekeeper/": change = 220]

---
+++ timekeeper/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Timekeeper")]
+[assembly: AssemblyDescription("Personal Time-Tracking Application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Technitivity")]
+[assembly: AssemblyProduct("Timekeeper")]
+[assembly: AssemblyCopyright("Copyright © 1999-2012 by Technitivity")]
+[assembly: AssemblyTrademark("Technitivity is a Division of Lockshire Media, LLC")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca66ee4a-32df-4293-909b-3ff44ad3ef0c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.3.0.1")]
+[assembly: AssemblyFileVersion("2.3.0.1")]
