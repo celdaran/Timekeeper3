0dd7df19cc1232d260de170a5c0f5d20b03be844
Charlie Hills <github@phizzy.com>
2014-08-11 06:40:15 -0500

    Updated version to 3.0.513.1.
    -----------------------------------------------------------------------
    TESTING
    -----------------------------------------------------------------------
    
    - Take this to work.
    - Give it a whirl.
    
    -----------------------------------------------------------------------
    DETAILS
    -----------------------------------------------------------------------
    
    - Straightforward.
    
    -----------------------------------------------------------------------
    WORKFLOW
    -----------------------------------------------------------------------
    
    Project Name....: Timekeeper
    Components......: n/a
    Target Version..: 3.0
    Type of Change..: Alpha Release
    
    Tickets Addressed:
      None
    
    Tickets Closed:
      None

--- timekeeper/Properties/AssemblyInfo.cs
+++ timekeeper/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.0.513.0")]
-[assembly: AssemblyFileVersion("3.0.513.0")]
+[assembly: AssemblyVersion("3.0.513.1")]
+[assembly: AssemblyFileVersion("3.0.513.1")]
