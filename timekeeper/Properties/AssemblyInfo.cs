df7466f484a989fa3f48b15546bf281f488565c6
hillsc <atlassian.b23d@tmx.email>
2021-03-08 09:38:40 -0600

    Set new version info for upcoming 3.2 release

--- timekeeper/Properties/AssemblyInfo.cs
+++ timekeeper/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Technitivity")]
 [assembly: AssemblyProduct("Timekeeper")]
-[assembly: AssemblyCopyright("Copyright © 1999-2019 by Technitivity")]
+[assembly: AssemblyCopyright("Copyright © 1999-2020 by Technitivity")]
 [assembly: AssemblyTrademark("Technitivity is a Division of Lockshire Media, LLC")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.101.1")]
-[assembly: AssemblyFileVersion("3.1.101.1")]
+[assembly: AssemblyVersion("3.2.102.1")]
+[assembly: AssemblyFileVersion("3.2.102.1")]
